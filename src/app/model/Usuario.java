439829a5d5329da8b71e32ca27caf17eeadb4bfa
leonohub <mendoncal717@gmail.com>
2025-12-22 21:01:53 -0300

    Added the User model

--- src/app/model/Usuario.java
+++ src/app/model/Usuario.java
@@ -1,5 +1,31 @@
 package app.model;
 
 public class Usuario {
-        
+        private int id;
+        int age;
+        String name;
+        String email;
+
+        public Usuario (int id, int age, String name, String email) {
+                this.id = id;
+                this.age = age;
+                this.name = name;
+                this.email = email;
+        }
+
+        public int getAge() {
+                return age;
+        }
+
+        public String getEmail() {
+                return email;
+        }
+
+        public int getId() {
+                return id;
+        }
+
+        public String getName() {
+                return name;
+        }
 }
