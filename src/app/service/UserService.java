28b2069a3d8e4aa0a2d8e500fc6813a9a7a1891e
leonohub <mendoncal717@gmail.com>
2025-12-22 21:08:52 -0300

    Created the register method POST

--- src/app/service/UserService.java
+++ src/app/service/UserService.java
@@ -1,5 +1,16 @@
 package app.service;
 
+import app.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserService {
-        
+
+        private static List<User> users = new ArrayList<>();
+
+        static void register (int id, int age, String name, String email) {
+                User user = new User(id, age, name, email);
+                users.add(user);
+        }
 }
